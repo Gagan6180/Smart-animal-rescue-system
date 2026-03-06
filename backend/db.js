8355cf3defd3ddca28b53d21688f33e8adb31492
Tusshar Singh <tusshar580@gmail.com>
2026-03-06 16:02:07 +0530

    Clear MongoDB connection string
    Updated MongoDB connection string to an empty value.

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://upadhyayagagan2002_db_user:teamgoogle@cluster0.t2oyfxw.mongodb.net/fureverSafeDB");
+    await mongoose.connect("");
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
